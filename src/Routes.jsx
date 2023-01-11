f016a961bf1448dfc2acab5e0e0aca7007c16de1
IrisGomis <gomis.f.iris@gmai.com>
2023-01-11 13:05:48 +0100

    added Routes

---
+++ src/Routes.jsx
@@ -0,0 +1,29 @@
+/*import * as React from "react";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import NavBar from './components/nav/NavBar';
+import Footer from './components/footer/Footer';
+import Boton from './components/boton/Boton';
+import Sound from './components/pages/Sound';
+import Discovery from './components/pages/Discovery';
+import Join from './components/pages/Join';
+import Azul from './components/circulos/Azul';
+import Cuarto from './components/circulos/Cuarto';
+import Azul from './components/circulos/Rosa';
+const Rutas = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/NavBar" element={<NavBar/>}/>
+                <Route path="/Footer" element={<Footer/>}/>
+                <Route path="/Boton" element={<Boton/>}/>
+                <Route path="/Sound" element={<Sound/>}/>
+                <Route path="/Discovery" element={<Discovery/>}/>
+                <Route path="/Join" element={<Join/>}/>
+                <Route path="/Azul" element={<Azul/>}/>
+                <Route path="/Cuarto" element={<Cuarto/>}/>
+                <Route path="/Rosa" element={<Rosa/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default Rutas*/
