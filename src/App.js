cb662b227f17c0b58f3c955e6808800497e92f9d
anagararna <anagararna@gmail.com>
2023-01-18 14:40:50 +0100

    borrando home de app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import "./App.css";
 import Routes from "./Routes";
 import {BrowserRouter } from "react-router-dom";
-import Home from "./components/pages/Home";
 
 function App() {
   return (
