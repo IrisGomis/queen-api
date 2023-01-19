56bf98a6769b09335eba28cb21164ce2c204feef
georgettenpm <georgette949@hotmail.es>
2023-01-19 09:49:05 +0100

    descargando cambios de dev

---
+++ src/components/templates/Wash.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../images/wash.png"
+
+
+
+function Wash() {
+  return (
+    <div className='container'>
+      <p>Wash</p>
+      <h1>Wash your hands min 20 second</h1>
+      <div className='hands-image'>
+       <img src="../images/wash.png" alt="foto-hands"></img>  
+       </div>   
+    </div>
+  )
+}
+
+export default Wash
+
