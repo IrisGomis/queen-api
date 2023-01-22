2c793440421fe0c51b2c39e8c6911d3ee6ea7474
Sara Álvarez <saralvzlpz@gmail.com>
2023-01-22 21:47:52 +0100

    card tracker terminado: consumo api, card, estilos y responsive

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import "./App.css";
 import Routes from "./Routes";
 import {BrowserRouter } from "react-router-dom";
+import CardsTracker from "./components/pages/CardsTracker";
 
 function App() {
              
