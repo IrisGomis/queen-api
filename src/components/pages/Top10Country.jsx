bdfcc457915a6192a2fa13f604428661339435e7
unknown <marielayamilex@yahoo.es>
2023-01-23 08:35:08 +0100

    Cambios Top10Country

--- src/components/pages/Top10Country.jsx
+++ src/components/pages/Top10Country.jsx
@@ -6,17 +6,16 @@ import Top10CountryRigth from '../organismos/Top10CountryRigth';
 function Top10Country() {
   return (
     <main className="d-flex">
-    <section >
-      <NavBTracker/>
-    </section>  
-    <section className="nav-info bg-white">  
-      <CovidTracker/>
-    </section>
-    <section className="nav-info bg-white"> 
-      <Top10CountryRigth/>
-    </section>
-  </main>
-
+      <section >
+        <NavBTracker/>
+      </section>  
+      <section className="nav-info bg-white">  
+        <CovidTracker/>
+      </section>
+      <section className="nav-info bg-white"> 
+        <Top10CountryRigth/>
+      </section>
+    </main>
   )
 }
 
